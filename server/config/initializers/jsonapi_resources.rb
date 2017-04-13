dca4b72b28bf30349e30b8ffafeb891d3d461c95
tb <t.bak@selleo.com>
2017-04-13 21:37:44 +0200

    include total count

--- server/config/initializers/jsonapi_resources.rb
+++ server/config/initializers/jsonapi_resources.rb
@@ -3,6 +3,6 @@ JSONAPI.configure do |config|
   config.default_paginator = :none
   config.default_page_size = 10
   config.maximum_page_size = 999
-  config.top_level_meta_include_page_count = true
-  config.top_level_meta_page_count_key = :page_count
+  config.top_level_meta_include_record_count = true
+  config.top_level_meta_record_count_key = :total
 end
