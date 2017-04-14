469620390e5f90e122c4728f9c0fc02f17966faf
tb <t.bak@selleo.com>
2017-04-14 21:22:16 +0200

    remove not used selectors

--- client/src/store/api/selectors.js
+++ client/src/store/api/selectors.js
@@ -1,9 +1,6 @@
-import { get, map, keyBy, isEmpty } from 'lodash';
+import { get, isEmpty } from 'lodash';
 
-export const getOne = (state, key, id) => get(state, ['api', key, 'byId', id]) || {
-  attributes: {},
-  relationships: {},
-};
+export const getOne = (state, key, id) => get(state, ['api', key, 'byId', id]) || {};
 
 export const getMap = (state, key) => get(state, ['api', key, 'byId']) || {};
 
@@ -13,28 +10,6 @@ export const getMany = (state, key, ids) => {
   return isEmpty(byId)
     ? []
     : (ids || Object.keys(byId)).map(id => byId[id]);
-};
-
-export const getRelation = (state, key, id, relation) => {
-  const parent = getOne(state, key, id);
-
-  if (!parent.id) {
-    return {
-      attributes: {},
-      relationships: {},
-    };
-  }
-
-  const relationData = get(parent, ['relationships', relation, 'data']);
-
-  if (!relationData) {
-    return {
-      attributes: {},
-      relationships: {},
-    };
-  }
-
-  return getOne(state, relationData.type, relationData.id);
 };
 
 export const getList = (state, key) => {
