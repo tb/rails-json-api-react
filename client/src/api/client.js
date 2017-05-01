57341e44b6a2c52ea17eb1ebda1b814f93e2d832
tb <t.bak@selleo.com>
2017-05-01 16:34:53 +0200

    Redirect to login on 401

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -29,6 +29,16 @@ const client = axios.create({
   },
 });
 
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/#/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 client.interceptors.request.use(
   (config) => {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
