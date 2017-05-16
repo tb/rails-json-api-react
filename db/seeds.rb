f369b8a4d316a91d9654e1cf64c912184b3f8585
tb <t.bak@selleo.com>
2017-05-16 14:58:55 +0200

    Fix users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,10 @@ FactoryGirl.find_definitions
 end
 
 25.times do |n|
-  u = User.create!(email: "user#{n}@example.com", password: 'Secret123', confirmed_at: Time.now)
-  u.add_role n == 0 ? :admin : :user
+  User.create!(
+    email: "user#{n}@example.com",
+    password: 'Secret123',
+    confirmed_at: Time.now,
+    tokens: nil
+  ).add_role n == 0 ? :admin : :user
 end
