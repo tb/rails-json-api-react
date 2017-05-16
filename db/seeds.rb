f2ee3ca8ffa28d0d837bf4265818e631f382295c
Dominik Felczak <dfelczak@live.com>
2017-05-16 15:03:13 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ FactoryGirl.find_definitions
 end
 
 25.times do |n|
-  u = User.create!(email: "user#{n}@example.com", password: 'Secret123', confirmed_at: Time.now)
+  u = User.create!(email: "user#{n}@example.com", password: 'Secret123', confirmed_at: Time.now, tokens: nil)
   u.add_role n == 0 ? :admin : :user
 end
